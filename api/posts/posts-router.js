fe5f1f7045bd50ba03720166da9bb04b608ff5ac
iamleahb <leah.ball@gmail.com>
2022-02-09 22:37:33 -0600

    put completes

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,5 +1,6 @@
 // implement your posts router here
 const express = require("express");
+const { restart } = require("nodemon");
 const Post = require("./posts-model");
 
 const router = express.Router();
@@ -77,7 +78,42 @@ router.delete("/:id", async (req, res) => {
     });
   }
 });
-router.put("/", (req, res) => {});
+router.put("/:id", (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res.status(400).json({
+      message: "please provide title and contents for this post",
+    });
+  } else {
+    Post.findById(req.params.id)
+      .then((stuff) => {
+        if (!stuff) {
+          res.status(404).json({
+            message: "The post with the specified ID does not exist",
+          });
+        } else {
+          return Post.update(req.params.id, req.body);
+        }
+      })
+      .then((data) => {
+        if (data) {
+          return Post.findById(req.params.id);
+        }
+      })
+      .then((post) => {
+        if (post) {
+          res.json(post);
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({
+          message: "The posts information could not be retrieved",
+          err: err.message,
+          stack: err.stack,
+        });
+      });
+  }
+});
 router.get("/:id/messages", (req, res) => {});
 
 module.exports = router;
